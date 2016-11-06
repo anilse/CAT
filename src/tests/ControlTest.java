7878e4f7187848a400db23f6c83956dd050fad1a
anils <anil.sezgin@outlook.com>
2016-11-06 23:22:07 +0400

    Added structure for unit tests

---
+++ src/tests/ControlTest.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import junit.framework.TestCase;
+
+
+/**
+ * @author anilse
+ *
+ */
+public class ControlTest extends TestCase{
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
