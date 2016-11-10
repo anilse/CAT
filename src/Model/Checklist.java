0b6e2c2af8ad1d4dcc5d993c4a037a8652eace23
nergiscelen <nergis.celen@vestel.com.tr>
2016-11-10 23:52:14 +0300

    Checklist class is edited according to DV requirements.
    Additional items are added to class

--- src/Model/Checklist.java
+++ src/Model/Checklist.java
@@ -1,61 +1,74 @@
 package Model;
 
+import java.util.HashMap;
 import java.util.Map;
 
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
-
-
-@XmlRootElement
 public class Checklist {
 
     String checklist_item;
     String[] function;
-    Map<String, String> check_steps;
-    String[] check_step;
-    String status;
-
-    public Checklist(String checklist_item, String[] function, Map<String, String> check_steps, String status) {
-        this.checklist_item = checklist_item;
-        this.function = function;
-        this.check_steps = check_steps;
-        this.status = status;
-    }
+    String[] test_method;
+    String criteria;
+    Map<String, String> test_info;
+    String[] status;
 
     public Checklist() {
+    }
 
+    public Checklist(String checklist_item, String[] function, String[] test_method, String criteria, Map<String, String> test_info, String[] status) {
+        this.checklist_item = checklist_item;
+        this.function = function;
+        this.test_method = test_method;
+        this.criteria = criteria;
+        this.test_info = test_info;
+        this.status = status;
     }
 
     public String getChecklist_item() {
         return checklist_item;
     }
 
-    @XmlElement
     public void setChecklist_item(String checklist_item) {
         this.checklist_item = checklist_item;
     }
 
+    public String[] getFunction() {
+        return function;
+    }
 
     public void setFunction(String[] function) {
         this.function = function;
     }
 
-    public Map<String, String> getCheck_steps() {
-        return check_steps;
+    public String[] getTest_method() {
+        return test_method;
     }
 
-    @XmlElement
-    public void setCheck_steps(Map<String, String> check_steps) {
-
-       //String check_step =  check_steps.get(function);
+    public void setTest_method(String[] test_method) {
+        this.test_method = test_method;
     }
 
-    public String getStatus() {
+    public String getCriteria() {
+        return criteria;
+    }
+
+    public void setCriteria(String criteria) {
+        this.criteria = criteria;
+    }
+
+    public Map<String, String> getTest_info() {
+        return test_info;
+    }
+
+    public void setTest_info(Map<String, String> test_info) {
+        this.test_info = test_info;
+    }
+
+    public String[] getStatus() {
         return status;
     }
 
-    @XmlElement
-    public void setStatus(String status) {
+    public void setStatus(String[] status) {
         this.status = status;
     }
 }
