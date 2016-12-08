4a0f1c8812c59d8f99291ff0ae28fe854726203d
nergiscelen <nergis.celen@vestel.com.tr>
2016-12-08 21:01:31 +0200

    Radio buttons are replaced with run buttons

--- src/View/View.java
+++ src/View/View.java
@@ -87,42 +87,42 @@ public class View extends JPanel {
             }
         });
     }
-    
-    public void drawTestRunner(){
 
-    	
-    	ImageIcon icon = new ImageIcon("pic1.jpg");  
-        JLabel label = new JLabel();  
-         
-        ImageIcon icon2 = new ImageIcon("pic2.jpg");  
-        JLabel label2 = new JLabel(); 
-    	
-		ImageIcon icon3 = new ImageIcon("na.jpg");  
-        JLabel label3 = new JLabel();  
-         
+    public void drawTestRunner() {
+
+
+        final ImageIcon icon = new ImageIcon("pic1.jpg");
+        final JLabel label = new JLabel();
+
+        final ImageIcon icon2 = new ImageIcon("pic2.jpg");
+        final JLabel label2 = new JLabel();
+
+        final ImageIcon icon3 = new ImageIcon("na.jpg");
+        final JLabel label3 = new JLabel();
+
         JButton btnAddFlight = new JButton("CLOSE");
         btnAddFlight.setBounds(220, 400, 220, 30);
-        
-        JButton resultbutton = new JButton("GET RESULT");
+
+        JButton resultbutton = new JButton("RUN ALL");
         resultbutton.setBounds(450, 400, 220, 30);
-        
-        JFrame window = new JFrame("CAT Result Table"); 
-        
-        JRadioButton t[] = new JRadioButton[Control.count_f_total];
-        
- //       ButtonGroup buttonGroup = new ButtonGroup();
-        
+
+        final JFrame window = new JFrame("CAT Result Table");
+
+        final JButton t[] = new JButton[Control.count_f_total];
+
+        //       ButtonGroup buttonGroup = new ButtonGroup();
+
         int x;
-        int offset =180;
-        for (x = 0 ; x < Control.count_f_total ; x++ )  {
-        	t[x] = new JRadioButton("");
-            t[x].setBounds(570, offset, 18, 18);
+        int offset = 180;
+        for (x = 0; x < Control.count_f_total; x++) {
+            t[x] = new JButton("RUN");
+            t[x].setBounds(570, offset, 70, 20);
 //            buttonGroup.add(t[x]);
             window.add(t[x]);
             offset += 16;
         }
-        
-        
+
+
         window.add(btnAddFlight);
         window.add(resultbutton);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -133,7 +133,7 @@ public class View extends JPanel {
         window.setVisible(true);
         window.setLocationRelativeTo(null);
 
-        
+
         btnAddFlight.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 System.out.println("CLOSE Button clicked.");
@@ -141,22 +141,22 @@ public class View extends JPanel {
             }
         });
 
-        
+
         resultbutton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-            	// Initiate the icons first.
-                label3.setEnabled(false); 
+                // Initiate the icons first.
+                label3.setEnabled(false);
                 label2.setEnabled(false);
                 label.setEnabled(false);
                 label.setIcon(null);
                 label2.setIcon(null);
                 label3.setIcon(null);
-            	int k = 0;
-            	int check = 0;
-            	for (k = 0 ; k < Control.count_f_total ; k++) {
-            		if (t[k].isSelected() ) check++;
-            	}
-            	if (check == 0){
+                int k = 0;
+                int check = 0;
+                for (k = 0; k < Control.count_f_total; k++) {
+                    if (t[k].isSelected()) check++;
+                }
+                if (check == 0) {
                     label3.setIcon(icon3);
                     label3.setBounds(300, 400, 5, 5);
                     label3.setVisible(true);
@@ -166,39 +166,47 @@ public class View extends JPanel {
                     window.add(label3, BorderLayout.EAST);
                     window.revalidate();
                     Control.test_status = 2;
-	            } else {
+                } else {
                     window.remove(label3);
-	            	if (check == Control.count_f_total) {
-	            	//	 window.dispatchEvent(new WindowEvent(window, WindowEvent.WINDOW_CLOSING));
-	                    label.setIcon(icon);
-	                    label.setBounds(300, 400, 5, 5);
-	                    label.setVisible(true);
-	                    label.setEnabled(true);
-	                    window.remove(label2);
-	                    window.add(label, BorderLayout.EAST);
-	                    window.revalidate();
-	                    Control.test_status = 0;
-	            	
-	            	} else {
-	            //		 window.dispatchEvent(new WindowEvent(window, WindowEvent.WINDOW_CLOSING));
-	                    label2.setIcon(icon2);
-	                    label2.setBounds(300, 400, 5, 5);
-	                    label2.setVisible(true);
-	                    label2.setEnabled(true);
-	                    window.remove(label);
-	                    window.add(label2, BorderLayout.EAST);
-	                    window.revalidate();
-	                    Control.test_status = 1;
-	            	}
-	                label.updateUI();
-	                label2.updateUI();
-	               
-	            }
-                switch (Control.test_status){
-	                case 0: System.out.println("test_status is PASS"); break;
-	                case 1: System.out.println("test_status is FAIL"); break;
-	                case 2: System.out.println("test_status is NA"); break;
-	                default: System.out.println("A weird test_status"); break;
+                    if (check == Control.count_f_total) {
+                        //	 window.dispatchEvent(new WindowEvent(window, WindowEvent.WINDOW_CLOSING));
+                        label.setIcon(icon);
+                        label.setBounds(300, 400, 5, 5);
+                        label.setVisible(true);
+                        label.setEnabled(true);
+                        window.remove(label2);
+                        window.add(label, BorderLayout.EAST);
+                        window.revalidate();
+                        Control.test_status = 0;
+
+                    } else {
+                        //		 window.dispatchEvent(new WindowEvent(window, WindowEvent.WINDOW_CLOSING));
+                        label2.setIcon(icon2);
+                        label2.setBounds(300, 400, 5, 5);
+                        label2.setVisible(true);
+                        label2.setEnabled(true);
+                        window.remove(label);
+                        window.add(label2, BorderLayout.EAST);
+                        window.revalidate();
+                        Control.test_status = 1;
+                    }
+                    label.updateUI();
+                    label2.updateUI();
+
+                }
+                switch (Control.test_status) {
+                    case 0:
+                        System.out.println("test_status is PASS");
+                        break;
+                    case 1:
+                        System.out.println("test_status is FAIL");
+                        break;
+                    case 2:
+                        System.out.println("test_status is NA");
+                        break;
+                    default:
+                        System.out.println("A weird test_status");
+                        break;
                 }
             }
         });
